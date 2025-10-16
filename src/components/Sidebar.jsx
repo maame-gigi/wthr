781bece7c486e011301c232e2e1dba1dd4679387
maame-gigi <riamaameadom@gmail.com>
2025-10-16 12:15:02 +0000

    changed colors on dashboard pagge

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,16 +7,16 @@ function Sidebar() {
 
   return (
     <div
-      className={`bg-gray-750 text-white h-screen p-4 transition-all duration-300 ${
+      className={`bg-yellow-500 text-white font-bold h-screen p-4 transition-all duration-300 ${
         isOpen ? "w-64" : "w-20"
       } flex flex-col`}
     >
       {/* Header + Toggle */}
       <div className="flex items-center justify-between mb-6">
-        {isOpen && <h1 className="text-2xl font-bold mb-10 ml-6">WTHR</h1>}
+        {isOpen && <h1 className="text-2xl text-gray-950 font-bold mb-10 ml-6">WTHR</h1>}
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="p-2 rounded hover:bg-gray-600"
+          className="p-2 rounded hover:bg-gray-950"
         >
           <Menu size={22} />
         </button>
@@ -26,26 +26,26 @@ function Sidebar() {
       <nav className="space-y-3 text-sm">
         <Link
           to="/"
-          className="block py-2 px-3 rounded hover:bg-gray-800 transition"
+          className="block py-6 px-9 rounded hover:bg-black-800 transition"
         >
-          {isOpen ? "Weather Now" : "☀️⛅🌨️🌩️🌦️"}
+          {isOpen ? "Weather Now" : "⛅🌩️🌦️"}
         </Link>
         <Link
           to="/forecast"
-          className="block py-6 px-9 rounded hover:bg-gray-800 transition"
+          className="block py-6 px-9 rounded hover:bg-black-800 transition"
         >
-          {isOpen ? "Activity Generator" : "🤸‍♀️⛷️🧗‍♀️🏊🎲"}
+          {isOpen ? "Activity Generator" : "🤸‍♀️⛷️🏊"}
         </Link>
         <Link
           to="/lifestyle"
-          className="block py-6 px-9 rounded hover:bg-gray-800 transition"
+          className="block py-6 px-9 rounded hover:bg-black-800 transition"
         >
-          {isOpen ? "Outfit Shuffle" : "👕👖🥼🧤🧦"} 
+          {isOpen ? "Outfit Shuffle" : "👕👖🧦"} 
         </Link>
       </nav>
 
       {/* Footer */}
-      <div className="mt-auto text-center text-xs text-gray-500">
+      <div className="mt-auto text-center text-xs text-gray-950">
         {isOpen && <p>COPYRIGHT 2025</p>}
       </div>
     </div>
