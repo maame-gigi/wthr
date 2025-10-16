79695b0de04c2396fb12ed6a9afa2f5e74bb5ecf
maame-gigi <riamaameadom@gmail.com>
2025-10-16 10:17:02 +0000

    updated dashborad/landing page color

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -1,12 +1,7 @@
-// Forecast.jsx
 import React from "react";
 
 function Forecast() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-600 via-indigo-400 to-blue-300 flex items-center justify-center p-4 text-white">
-      <h1 className="text-3xl font-bold">Forecast (Yesterday • Today • Tomorrow)</h1>
-    </div>
-  );
+  return <div className="text-xl">Forecast Page</div>;
 }
 
 export default Forecast;
--- src/pages/Lifestyle.jsx
+++ src/pages/Lifestyle.jsx
@@ -1,12 +1,7 @@
-// Lifestyle.jsx
 import React from "react";
 
 function Lifestyle() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-pink-500 via-red-400 to-orange-300 flex items-center justify-center p-4 text-white">
-      <h1 className="text-3xl font-bold">Outfit & Activity Recommendations 👕🏃‍♀️</h1>
-    </div>
-  );
+  return <div className="text-xl">Recommendations Page</div>;
 }
 
 export default Lifestyle;
